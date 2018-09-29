46f6204228c6794585aaa55eda07e35bf6599d25
SangKil Park <ht1911@gmail.com>
2018-09-29 09:48:02 +0900

    poiner

---
+++ pointer.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+// Alias *int to intAddress for clarity
+type intAddress *int
+// addressOf takes an integer and returns the memory address
+// of that integer.
+func addressOf(i int) intAddress {
+  return &i
+}
+// valueOf takes an intAddress, and returns the value
+// at that address.
+func valueOf(ia intAddress) int {
+  return *ia
+}
+func main() {
+  x := 4
+  y := addressOf(x)
+  z := valueOf(y)
+  fmt.Printf("y: %+v\n", y)
+  fmt.Printf("z: %d\n", z)
+}
