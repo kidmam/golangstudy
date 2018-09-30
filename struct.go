aa910fa747d2966ba6aa4904abbcfdfc80baa923
SangKil Park <ht1911@gmail.com>
2018-09-30 20:12:15 +0900

    struct, 생성자(constructor) 함수

--- struct.go
+++ struct.go
@@ -7,10 +7,35 @@ type person struct {
 	age  int
 }
 
+type dict struct {
+	data map[int]string
+}
+
+func newDict() *dict {
+	d := dict{}
+	d.data = map[int]string{}
+	return &d
+}
+
 func main() {
 	p := person{}
 	p.name = "Lee"
 	p.age = 10
 
+	var p1 person
+	p1 = person{"Bob", 20}
+	p2 := person{name: "Sean", age: 50}
+
 	fmt.Println(p)
+	fmt.Println(p1)
+	fmt.Println(p2)
+
+	p3 := new(person)
+	p3.name = "Lee" // p가 포인터라도 . 을 사용한다
+	fmt.Println(*p3)
+
+	dic := newDict() // 생성자 호출
+	dic.data[1] = "A"
+	fmt.Println(dic.data[1])
+
 }
