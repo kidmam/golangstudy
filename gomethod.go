ab9893ea648450a7296bb3c8d8de27ec64dddad5
SangKil Park <ht1911@gmail.com>
2018-10-02 11:00:13 +0900

    Value receiver는 struct의 데이타를 복사(copy)하여 전달하며, 포인터 receiver는 struct의 포인터만을 전달한다.

--- gomethod.go
+++ gomethod.go
@@ -12,8 +12,17 @@ func (r Rect) area() int {
 	return r.width * r.height
 }
 
+// 포인터 Receiver
+func (r *Rect) area2() int {
+	r.width++
+	return r.width * r.height
+}
+
 func main() {
 	rect := Rect{10, 20}
 	area := rect.area() //메서드 호출
 	fmt.Println(area)
+
+	area2 := rect.area2()      //메서드 호출
+	println(rect.width, area2) // 11 220 출력
 }
