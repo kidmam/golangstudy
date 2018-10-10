58f637c4645a8bf9f78d629b989c370978a494ba
SangKil Park <ht1911@gmail.com>
2018-10-11 05:06:05 +0900

    Request시 보다 많은 컨트롤이 필요하다면, Request 객체를 (NewRequest 생성자를 통해) 직접 생성해서 http.Client 객체를 통해 호출

---
+++ gohttpgetheader.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	// Request 객체 생성
+	req, err := http.NewRequest("GET", "http://csharp.tips/feed/rss", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	//필요시 헤더 추가 가능
+	req.Header.Add("User-Agent", "Crawler")
+
+	// Client객체에서 Request 실행
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// 결과 출력
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	str := string(bytes) //바이트를 문자열로
+	fmt.Println(str)
+}
