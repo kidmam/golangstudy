0f52b43d9420ddbbdba52f7baa9d62bafc99b2d4
SangKil Park <ht1911@gmail.com>
2018-10-22 05:41:03 +0900

    Go에서 로그를 사용하기 위하여 Go의 표준 패키지 "log"를 이용할 수 있다.

---
+++ gologging.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+var myLogger *log.Logger
+
+func main() {
+	myLogger = log.New(os.Stdout, "INFO: ", log.LstdFlags)
+
+	//....
+	run()
+
+	myLogger.Println("End of Program")
+}
+
+func run() {
+	myLogger.Print("Test")
+}
