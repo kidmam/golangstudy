807ed0484699d2b994f4259b5c3a9623e52d1fd7
SangKil Park <ht1911@gmail.com>
2019-01-01 12:52:31 +0900

    error

---
+++ gomaster/newError.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"log"
+	"os"
+)
+
+func returnError(a, b int) error {
+	if a == b {
+		err := errors.New("Error in returnError() function!")
+		return err
+	} else {
+		return nil
+	}
+}
+
+func main() {
+	err := returnError(1, 2)
+	if err == nil {
+		fmt.Println("returnError() ended normally")
+	} else {
+		fmt.Println(err)
+		log.Println(err)
+		//panic(err)
+		os.Exit(10)
+	}
+
+	err = returnError(10, 10)
+	if err == nil {
+		fmt.Println("returnError() ended normally")
+	} else {
+		fmt.Println(err)
+	}
+
+	if err.Error() == "Error in returnError() function!" {
+		fmt.Println("!!")
+	}
+
+}
