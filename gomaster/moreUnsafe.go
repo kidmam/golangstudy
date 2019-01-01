ff7341ed3d9b4afdbc1dfea648f9901f67cfbd8c
SangKil Park <ht1911@gmail.com>
2019-01-02 05:50:02 +0900

    unsafe package

---
+++ gomaster/moreUnsafe.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	array := [...]int{0, 1, -2, 3, 4}
+	pointer := &array[0]
+	fmt.Println(*pointer, " ")
+
+	memoryAddress := uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
+	fmt.Println("memoryAddress: ", memoryAddress)
+
+	for i := 0; i < len(array)-1; i++ {
+		pointer = (*int)(unsafe.Pointer(memoryAddress))
+		fmt.Println(*pointer, " ")
+		memoryAddress = uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
+	}
+
+	fmt.Println()
+	pointer = (*int)(unsafe.Pointer(memoryAddress))
+	fmt.Println("One More: ", *pointer, " ")
+	memoryAddress = uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
+	fmt.Println()
+}
