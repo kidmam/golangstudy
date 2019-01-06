99883667b11a08d3356b48d31c3cb2d45287d27b
SangKil Park <ht1911@gmail.com>
2019-01-06 16:05:33 +0900

    interface

--- gointerface.go
+++ gointerface.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	. "github.com/kidmam/golangstudy/util"
 )
 
@@ -11,9 +12,28 @@ func showArea(shapes ...Shape) {
 	}
 }
 
+// 인터페이스 정의
+type SomeBehivor interface {
+	DoSomething(arg string) string
+}
+
+// 구조체 정의
+type StructB struct {
+}
+
+// 인터페이스 구현
+func (self *StructB) DoSomething(arg string) string {
+	return arg
+}
+
 func main() {
 	r := RectNew{10., 20.}
 	c := Circle{10}
 
 	showArea(r, c)
+
+	// 인터페이스 타입 변수에 저장할 수 있다
+	var behivor SomeBehivor
+	behivor = &StructB{}
+	fmt.Println(behivor.DoSomething("A"))
 }
