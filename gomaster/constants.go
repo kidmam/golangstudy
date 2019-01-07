09120b6ab7d2ad7ac8776f7db0e13c69987b7c9b
SangKil Park <ht1911@gmail.com>
2019-01-07 19:42:37 +0900

    constants

---
+++ gomaster/constants.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Digit int
+type Power2 int
+
+const PI = 3.1415926
+
+const (
+	C1 = "adfadfasdf"
+	C2 = "adfadfasdf"
+	C3 = "adfadfasdf"
+)
+
+func main() {
+	const s1 = 123
+	const s2 float64 = 123
+	var v1 float32 = s1 * 12
+	//var v2 float32 = s2 * 12
+
+	fmt.Println(v1)
+	//fmt.Println(v2)
+}
