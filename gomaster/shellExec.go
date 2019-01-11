9fa49e8acf296df35d27eb9d1f6997c9b81cef8c
SangKil Park <ht1911@gmail.com>
2019-01-11 15:27:21 +0900

    go shell execute

--- gomaster/shellExec.go
+++ gomaster/shellExec.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	cmd := exec.Command("ls", "-lah")
+	/* cmd := exec.Command("./devnettool.sh")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
-	fmt.Printf("combined out:\n%s\n", string(out))
+	fmt.Printf("combined out:\n%s\n", string(out)) */
+
+	/*cmdStr := "devnettool.sh"
+	cmd := exec.Command("sh", "-c", cmdStr)
+	_, err := cmd.Output()
+
+	if err != nil {
+		println(err.Error())
+		return
+	}*/
+
+	cmdStr := "devnettool.sh"
+	cmd := exec.Command("sh", "-c", cmdStr)
+	cmdOutput := &bytes.Buffer{}
+	cmd.Stdout = cmdOutput
+	err := cmd.Run()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	fmt.Print(string(cmdOutput.Bytes()))
+
+	/*cmd := exec.Command("terraform", "version")
+	cmdOutput := &bytes.Buffer{}
+	cmd.Stdout = cmdOutput
+	err := cmd.Run()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	fmt.Print(string(cmdOutput.Bytes()))*/
 }
