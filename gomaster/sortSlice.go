d2360f804ee03bdb98c21b1aed97a392904273f6
SangKil Park <ht1911@gmail.com>
2019-01-14 20:49:09 +0900

    go struct

--- gomaster/sortSlice.go
+++ gomaster/sortSlice.go
@@ -14,6 +14,9 @@ type aStructure struct {
 func main() {
 	//p1 := aStructure{"person", 12, -2} 구조체 리터럴
 
+	/*var s1 aStructure
+	  s1.height = 20 */
+
 	mySlice := make([]aStructure, 0)
 	mySlice = append(mySlice, aStructure{"Mihalis", 180, 90})
 	mySlice = append(mySlice, aStructure{"Bill", 134, 45})
