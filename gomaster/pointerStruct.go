4b57d01c24ab68246328f66120c4024799357c91
SangKil Park <ht1911@gmail.com>
2019-01-20 12:24:02 +0900

    struct new

--- gomaster/pointerStruct.go
+++ gomaster/pointerStruct.go
@@ -1,5 +1,41 @@
 package main
 
+import "fmt"
+
+type myStructure struct {
+	Name    string
+	Surname string
+	Height  int32
+}
+
+func createStructure(n, s string, h int32) *myStructure {
+	if h > 300 {
+		h = 0
+	}
+	return &myStructure{n, s, h}
+}
+
+func retStructure(n, s string, h int32) myStructure {
+	if h > 300 {
+		h = 0
+	}
+	return myStructure{n, s, h}
+}
+
 func main() {
+	s1 := createStructure("길동", "홍", 123)
+	s2 := retStructure("길동", "홍", 123)
+
+	fmt.Println(s1)
+	fmt.Println((*s1).Name)
+
+	fmt.Println(s2)
+	fmt.Println(s2.Name)
+
+	pS := new(myStructure)
+	fmt.Println(pS)
+
+	sP := new([]myStructure)
+	fmt.Println(sP)
 
 }
