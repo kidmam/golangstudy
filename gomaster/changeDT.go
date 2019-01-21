0097e9f799d4540ad728a5c38c78e29bbc3336e0
SangKil Park <ht1911@gmail.com>
2019-01-21 20:59:15 +0900

    regexp

---
+++ gomaster/changeDT.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide one text file process!")
+		os.Exit(1)
+	}
+
+	filename := arguments[1]
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("error opening file %s", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	notAMatch := 0
+	r := bufio.NewReader(f)
+	for {
+		line, err := r.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Printf("error reading file %s", err)
+		}
+
+	}
+}
