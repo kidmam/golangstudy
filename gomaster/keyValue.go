bf3d506fb46a7c6ee04f56c5474410614f360606
SangKil Park <ht1911@gmail.com>
2019-02-02 14:23:07 +0900

    key value

--- gomaster/keyValue.go
+++ gomaster/keyValue.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type myElement struct {
 	Name    string
 	Surname string
@@ -8,17 +15,87 @@ type myElement struct {
 
 var DATA = make(map[string]myElement)
 
+func LOOKUP(k string) *myElement {
+	_, ok := DATA[k]
+	if ok {
+		n := DATA[k]
+		return &n
+	} else {
+		return nil
+	}
+}
+
 func ADD(k string, n myElement) bool {
 	if k == "" {
 		return false
 	}
 
-	if LOOKUP(k+ == nil) {
+	if LOOKUP(k) == nil {
 		DATA[k] = n
+		return true
 	}
 	return false
 }
 
+func DELETE(k string) bool {
+	if LOOKUP(k) != nil {
+		delete(DATA, k)
+		return true
+	}
+	return false
+}
+
+func CHANGE(k string, n myElement) bool {
+	DATA[k] = n
+	return true
+}
+
+func PRINT() {
+	for k, d := range DATA {
+		fmt.Printf("key: %s value: %v\n", k, d)
+	}
+}
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		text = strings.TrimSpace(text)
+		tokens := strings.Fields(text)
+
+		switch len(tokens) {
+		case 0:
+			continue
+		case 1:
+			tokens = append(tokens, "")
+			tokens = append(tokens, "")
+			tokens = append(tokens, "")
+			tokens = append(tokens, "")
+		case 2:
+			tokens = append(tokens, "")
+			tokens = append(tokens, "")
+			tokens = append(tokens, "")
+		case 3:
+			tokens = append(tokens, "")
+			tokens = append(tokens, "")
+		case 4:
+			tokens = append(tokens, "")
+		}
+
+		switch tokens[0] {
+		case "PRINT":
+			PRINT()
+		case "STOP":
+			return
+		case "ADD":
+			n := myElement{tokens[2], tokens[3], tokens[4]}
+			if !ADD(tokens[1], n) {
+				fmt.Println("ADD operation failed!")
+			}
+		default:
+			fmt.Println("Unknown command - please try again!")
+		}
+
+	}
 
 }
