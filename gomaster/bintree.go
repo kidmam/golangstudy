0006099fdbec292b103e265d28d277440246eef2
SangKil Park <ht1911@gmail.com>
2019-02-05 12:30:35 +0900

    binary tree

--- gomaster/bintree.go
+++ gomaster/bintree.go
@@ -1,9 +1,20 @@
 package main
 
+import "fmt"
+
 type Tree struct {
 	Left  *Tree
 	Value int
 	Right *Tree
+}
+
+func traverse(t *Tree) {
+	if t == nil {
+		return
+	}
+	traverse(t.Left)
+	fmt.Println(t.Value, " ")
+	traverse(t.Right)
 }
 
 func main() {
