76727392483a40e9b9afd2c849039ce5396f4147
SangKil Park <ht1911@gmail.com>
2019-02-09 05:15:58 +0900

    binary sort

--- gomaster/bintree.go
+++ gomaster/bintree.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Tree struct {
 	Left  *Tree
@@ -17,6 +21,41 @@ func traverse(t *Tree) {
 	traverse(t.Right)
 }
 
-func main() {
+func create(n int) *Tree {
+	var t *Tree
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < 2*n; i++ {
+		temp := rand.Intn(n * 2)
+		t = insert(t, temp)
+	}
+	return t
+}
 
+func insert(t *Tree, v int) *Tree {
+	if t == nil {
+		return &Tree{nil, v, nil}
+	}
+
+	if v == t.Value {
+		return t
+	}
+
+	if v < t.Value {
+		t.Left = insert(t.Left, v)
+		return t
+	}
+	t.Right = insert(t.Right, v)
+	return t
+}
+
+func main() {
+	tree := create(10)
+	fmt.Println("The value of the root of the tree is", tree.Value)
+	traverse(tree)
+	fmt.Println()
+	tree = insert(tree, -10)
+	tree = insert(tree, -2)
+	traverse(tree)
+	fmt.Println()
+	fmt.Println("The value of the root of the tree is", tree.Value)
 }
